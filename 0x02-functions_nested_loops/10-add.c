be601a7ae6fe7a7bfa590e7db9f9f2a4bdc594ce
Objayemmy <opeobajuluwa4@gmail.com>
2023-03-21 14:15:48 -0700

    a + b

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,18 +1,43 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- *   main - check the code
- * 
- *   Return: Always 0.
+ * print_times_table - Prints the times table of the input,
+ * starting with 0.
+ * @n: The value of the times table to be printed.
  */
 
-int main(void)
+void print_times_table(int n)
+
 {
-	    int n;
 
-	        n = add(89, 9);
-		printf("%d\n", n);
-		return (0);
+	int num, mult, prod;
 
+	if (n >= 0 && n <= 15)
+	{
+	for (num = 0; num <= n; num++)
+	{
+	_putchar('0');
+	for (mult = 1; mult <= n; mult++)
+	{
+	_putchar(',');
+	_putchar(' ');
+	prod = num * mult;
+	if (prod <= 99)
+	_putchar(' ');
+	if (prod <= 9)
+	_putchar(' ');
+	if (prod >= 100)
+	{
+	_putchar((prod / 100) + '0');
+	_putchar(((prod / 10)) % 10 + '0');
+	}
+	else if (prod <= 99 && prod >= 10)
+	{
+	_putchar((prod / 10) + '0');
+	}
+	_putchar((prod % 10) + '0');
+	}
+	_putchar('\n');
+	}
+	}
 }
