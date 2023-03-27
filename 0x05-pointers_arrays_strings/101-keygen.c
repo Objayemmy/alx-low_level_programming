e56616902a4a74188f1f1528c8845540377ba292
Objayemmy <opeobajuluwa4@gmail.com>
2023-03-27 14:30:08 -0700

    Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,7 +3,7 @@
 #include <time.h>
 
 /**
- * main - program that generates random valid
+ i* main - program that generates random valid
  * passwords for the program 101-crackme
  *
  * Return: Always 0 (Success)
@@ -32,5 +32,4 @@ int main(void)
 	}
 
 	return (0);
-}
 }
