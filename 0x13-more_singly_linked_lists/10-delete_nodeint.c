fff5acfe8c1c4eada1b167c1d9a6881b7755794c
Objayemmy <opeobajuluwa4@gmail.com>
2023-05-02 17:19:55 -0700

    Free (safe version)

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -11,6 +11,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *temp = *head;
 	listint_t *current = NULL;
+	unsigned int i = 0;
 
 	if (*head == NULL)
 		return (-1);
@@ -22,11 +23,14 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (1);
 	}
 
-	for (unsigned int i = 0; temp && i < index - 1; i++)
+	while (i < index - 1)
+	{
+		if (!temp || !(temp->next))
+			return (-1);
 		temp = temp->next;
+		i++;
+	}
 
-	if (!temp || !(temp->next))
-		return (-1);
 
 	current = temp->next;
 	temp->next = current->next;
