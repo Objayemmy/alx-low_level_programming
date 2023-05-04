87f4f463faa4612ad222b9375e73fc443dc54245
Objayemmy <opeobajuluwa4@gmail.com>
2023-05-04 11:20:04 -0700

    hundred

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -5,17 +5,13 @@
  * @n: pointer to the number to change
  * @index: index of the bit to set to 1
  *
- * Return: 1 for success, 0 for failure
+ * Return: 1 for success, -1 for failure
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	/* Check if index is within range */
 	if (index > 63)
-		return (0);
+		return (-1);
 
-	/* Set the bit at the specified index to 1 */
-	unsigned long int mask = (1UL << index);
-	*n |= mask;
-
+	*n = ((1UL << index) | *n);
 	return (1);
 }
--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -5,17 +5,13 @@
  * @n: pointer to the number to change
  * @index: index of the bit to clear
  *
- * Return: 1 for success, 0 for failure
+ * Return: 1 for success, -1 for failure
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	/* Check if index is within range */
 	if (index > 63)
-		return (0);
+		return (-1);
 
-	/* Clear the bit at the specified index to 0 */
-	unsigned long int mask = ~(1UL << index);
-	*n &= mask;
-
+	*n = (~(1UL << index) & *n);
 	return (1);
 }
